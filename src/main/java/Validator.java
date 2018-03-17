fa5d796dc8c735367f0ebb6ce69ee7c368218ff5
PPiedel <pawelpiedel@yahoo.pl>
2018-03-17 16:33:54 +0100

    Validator main implemented. Scanner,Parser classes removed.

--- src/main/java/Validator.java
+++ src/main/java/Validator.java
@@ -1,2 +1,16 @@
 public class Validator {
+
+    public static void main(String[] args) {
+        String input = "var query_orderby1 = from c in svcContext.ContactSet\n" +
+                "                      where !c.CreditLimit.Equals(null)\n" +
+                "                      orderby c.CreditLimit descending\n" +
+                "                      select new\n" +
+                "                      {\n" +
+                "                       limit = c.CreditLimit,\n" +
+                "                       first = c.FirstName,\n" +
+                "                       last = c.LastName\n" +
+                "                      }; ";
+
+        System.out.println(input);
+    }
 }
